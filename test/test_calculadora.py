3cc590a9b9edbbbb696f5c67850da7837bf16d71
Juan L. Restituyo <juanluisrestituyo@gmail.com>
2021-02-25 22:03:09 -0400

    adding api test

--- test/test_calculadora.py
+++ test/test_calculadora.py
@@ -1,14 +1,20 @@
 from pytest import mark
 from test import calculadora as calc
+import requests
 
 @mark.ui
 def test_suma():
     resultado = calc.suma(4,6)
     assert resultado == 10
 
-@mark.ui
-def test_suma2():
-    assert calc.suma(-3,-2) == -5
+@mark.parametrize("num1", [3,5,6,9])
+@mark.parametrize("num2", [1,3,4,5])
+
+@mark.suma
+def test_suma2(num1, num2):
+    resultado_esperado = num1 + num2
+    assert calc.suma(num1, num2) == resultado_esperado
+
 
 def test_resta():
     assert calc.resta(5,1) == 4
@@ -21,3 +27,10 @@ def test_division():
 
 def test_potenciacion():
     assert calc.potencia(3,2) == 9
+
+@mark.api
+def test_provincias():
+    url = "http://provinciasrd.raydelto.org/provincias/5"
+    response = requests.get(url).json()
+    claves = response.keys()
+    assert response['data'][0]['nombre'] == "Barahona"
